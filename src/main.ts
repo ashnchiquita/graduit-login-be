83af0d6d4aca030c9ce75f8e6893f0a452ef171d
Chiquita Ahsanunnisa <16521248@mahasiswa.itb.ac.id>
2024-03-30 03:23:50 +0700

    feat: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,20 @@ async function bootstrap() {
     origin: process.env.LOGIN_FE_URL,
     credentials: true,
   });
+
+  const options = new DocumentBuilder()
+    .setTitle("GraduIT API")
+    .setDescription(
+      "GraduIT API Documentation for account and authentication services",
+    )
+    .setVersion("1.0")
+    .addTag("auth")
+    .addTag("akun")
+    .addBearerAuth()
+    .addCookieAuth("gradu-it.access-token")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api-docs", app, document);
 
   await app.listen(Number(process.env.PORT));
 }
